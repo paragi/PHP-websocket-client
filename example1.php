42ca1f85950a799eb9de3bbf799141f472653035
Nabi KaramAliZadeh <NabiKAZ@gmail.com>
2020-05-24 17:19:23 +0430

    `persistant` set to true for basic example

--- example1.php
+++ example1.php
@@ -4,7 +4,7 @@ $server = 'echo.websocket.org';
 $message = "hello server";
 
 echo "Connecting to server: $server \n";
-if( $sp = websocket_open($server, 80,'',$errstr) ) {
+if( $sp = websocket_open($server, 80,'',$errstr, 10, false, true) ) {
   echo "Sending message to server: '$message' \n";
   websocket_write($sp,$message);
   echo "Server responed with: '" . websocket_read($sp,$errstr) ."'\n";
