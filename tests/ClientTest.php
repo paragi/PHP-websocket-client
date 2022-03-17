e1fa4dabf64c06adcf6be5df5c62e1b41b360d3d
Trismegiste <flo@trismegiste.net>
2022-03-17 10:36:37 +0100

    Refine tests

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,11 +1,15 @@
 <?php
 
+use Paragi\PhpWebsocket\Client;
+use Paragi\PhpWebsocket\ConnectionException;
+use PHPUnit\Framework\TestCase;
+
 /**
  * Test for Client
  *
  * @author Trismegiste
  */
-class ClientTest extends PHPUnit\Framework\TestCase
+class ClientTest extends TestCase
 {
 
     const testServer = 'echo.websocket.events';  // echo.websocket.org is no longer available
@@ -22,11 +26,24 @@ class ClientTest extends PHPUnit\Framework\TestCase
      */
     public function testExample(string $message)
     {
-        $sut = new \Paragi\PhpWebsocket\Client(self::testServer, 80, '', $errstr, 10, false);
+        $sut = new Client(self::testServer, 80, '', $errstr, 10, false);
         $written = $sut->write($message);
         $this->assertNotFalse($written, 'Unable to write to ' . self::testServer);
         $response = $sut->read($errstr);
         $this->assertStringContainsString($message, $response);
+    }
+
+    public function testUnknowHost()
+    {
+        $this->expectError(); // @todo perhaps this behavior should change, throw Exception ?
+        $this->expectErrorMessage('getaddrinfo failed');
+        new Client('yoloserver.unknown');
+    }
+
+    public function testNotAWebsocketServer()
+    {
+        $this->expectException(ConnectionException::class);
+        new Client('twitter.com');
     }
 
 }
